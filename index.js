497d0c93d0b6149e4827c46c48b7fa5dd4ac8487
Alan Mauricio <alanmauricioc@ouitlook.com>
2022-05-22 12:22:36 -0500

    check single thread behavior

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "Express";
+const app = express();
+
+function doWork(duration) {
+  const start = Date.now();
+  while (Date.now() - start < duration) {}
+}
+
+app.get("/", (_, res) => {
+  doWork(5000);
+  res.send("hi there");
+});
+
+app.listen(3000);
