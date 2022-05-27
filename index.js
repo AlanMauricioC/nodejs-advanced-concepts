acdd1b53ee3a7f3a0c0eb2b3e751f99dde8694fc
Alan Mauricio <alanmauricioc@ouitlook.com>
2022-05-26 19:12:48 -0500

    handle child and master clusters

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const app = express();
+const cluster = require("cluster");
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+if (cluster.isMaster) {
+  //cause index.js to be executed *again* but
+  // in child mode
+  cluster.fork();
+} else {
+  //server related code
+  // I'm a child c:
+
+  const express = require("express");
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get("/", (_, res) => {
+    doWork(5000);
+    res.send("hi there");
+  });
+
+  app.listen(3000);
 }
-
-app.get("/", (_, res) => {
-  doWork(5000);
-  res.send("hi there");
-});
-
-app.listen(3000);
