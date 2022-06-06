8941b1ff601eea2bdf08a4dda2fc66ac3ac4ddd7
Alan Mauricio <alanmauricioc@ouitlook.com>
2022-06-05 23:40:01 -0500

    start delegation to pm2

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-const cluster = require("cluster");
+const crypto = require("crypto");
+const express = require("express");
+const app = express();
 
-if (cluster.isMaster) {
-  //cause index.js to be executed *again* but
-  // in child mode
-  cluster.fork();
-  cluster.fork();
-} else {
-  //server related code
-  // I'm a child c:
-
-  const crypto = require("crypto");
-  const express = require("express");
-  const app = express();
-
-  app.get("/", (_, res) => {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-      res.send("hi there");
-    });
+app.get("/", (_, res) => {
+  crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    res.send("hi there");
   });
+});
 
-  app.get("/fast", (_, res) => {
-    res.send("this was fast there");
-  });
+app.get("/fast", (_, res) => {
+  res.send("this was fast there");
+});
 
-  app.listen(3000);
-}
+app.listen(3000);
